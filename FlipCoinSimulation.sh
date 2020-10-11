e060dae6277958d579bc8de392b696e68561c62c
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-12 00:27:59 +0530

    Added : Game Continued OR Game Over

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -4,17 +4,14 @@
 Head=0
 Tail=0
 
-while [ $Head -lt 21 ] && [ $Tail -lt 21 ] 
+while [ $Head -lt 21 ] && [ $Tail -lt 21 ]
 do
 Coin=$((1+RANDOM%2))
-
 if [ $Coin -eq 1 ]
 then
        ((Head++))
-
 elif [ $Coin -eq 2 ]
 then
-
     echo "It is an TAIL"
        ((Tail++))
 fi
@@ -27,15 +24,20 @@ then
 
 elif [ $Tail -eq 21 ]
 then
-
       Ratio=$(($Tail - $Head))
       echo "Tail win by $Ratio Times Then Head"
-
 else
 
-      echo "Hurray!!!! Its a Tie"
-
+while [ $Head -eq $tail ]
+do
+     echo "Now Only Game Can be Continued"
+    if [ $Head -gt 22 ] && [ $Tail -gt 22]
+    then
+         echo "It may be Posiible That Game Can be Continued"
+    else
+      echo "Game Over"
+    fi
+done
 fi
-
 echo "No Of Head Won" : $Head
 echo "No oF Tail Won" : $Tail
