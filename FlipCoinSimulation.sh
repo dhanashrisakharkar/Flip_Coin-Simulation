91340737289a731123980a071d1c598618bd548b
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-11 23:44:19 +0530

    Added : Flip Coin Head Or Tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -x
 
+Coin=$((1+RANDOM%3))
 
-echo "Welcome To Flip Coin Simulation"
+if [ $Coin -eq 1 ]
+then
+
+    echo "It is an HEAD"
+
+elif [ $Coin -eq 2 ]
+then
+
+    echo "It is an TAIL"
+
+else
+
+    echo "It is Not in Choice"
+
+fi
