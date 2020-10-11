ca924c8ae61327097df5748e2c9748c59a8273c9
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-11 23:55:40 +0530

    Added : No of Time Head or Tail Wins

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,19 +1,26 @@
 #!/bin/bash -x
 
-Coin=$((1+RANDOM%3))
+
+Head=0
+Tail=0
+
+for ((i=1; i<25; i++))
+do
+Coin=$((1+RANDOM%2))
 
 if [ $Coin -eq 1 ]
 then
 
     echo "It is an HEAD"
+       ((Head++))
 
 elif [ $Coin -eq 2 ]
 then
 
     echo "It is an TAIL"
-
-else
-
-    echo "It is Not in Choice"
-
+       ((Tail++))
 fi
+done
+
+echo "No Of Head Won" : $Head
+echo "No oF Tail Won" : $Tail
